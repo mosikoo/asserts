83582201a76859b7fb134b257c58f2ebc9cc5c6a
mosikoo <chenqiuws@gmail.com>
2018-07-20 12:08:45 +0800

    update: script

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/sh
+
+# 日常
+# export BUILD_HASH=daily && sh index.sh
+# 生产
+# sh index.sh
+
+
+replaceFile() {
+  file=`find build/$1/index*.$1`
+  if [ ! -n "$file" ]; then
+    return;
+  fi
+  echo "replace \033[36m$file\033[0m to build/$1/index.$1"
+  mv $file build/$1/index.$1
+}
+
+tnpm install --production
+tnpm install ice-scripts@latest
+
+# todo def中区分环境变量
+tnpm run build
+
+if [ $BUILD_HASH = "daily" ]; then
+  replaceFile "js"
+  replaceFile "css"
+fi
+
+mv build .package
+
+
